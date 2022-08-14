0e5e43035448b2e9aad56c1ea4c397781cd8bab4
Nicholas <nicholasjoven@gmail.com>
2022-08-14 19:42:51 -0400

    commit tree 1cd1a69e4a12b4f5d38836621e8a2281e397368a

---
+++ lib/insert.sql
@@ -0,0 +1,73 @@
+INSERT INTO bears VALUES (
+    1,
+    "Mr. Chocolate",
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    2,
+    "Rowdy",
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    3,
+    "Tabitha",
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    4,
+    "Sergeant Brown",
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    5,
+    "Melissa",
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    6,
+    "Grinch",
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    7,
+    "Wendy",
+    1,
+    "F",
+    "Brown",
+    "Bearish",
+    1
+);
+INSERT INTO bears VALUES (
+    8,
+    null,
+    1,
+    "M",
+    "Brown",
+    "Bearish",
+    1
+);
+
